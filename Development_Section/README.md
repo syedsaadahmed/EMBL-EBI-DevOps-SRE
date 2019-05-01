20a29e672aa01b9bcf5b7c2097b916e18a1263c1
syedsaadahmed <syedsaadahmed2094@yahoo.com>
2019-05-02 00:17:39 +0500

    Addition of all the code and documentation of REST-API

---
+++ Development_Section/README.md
@@ -0,0 +1,24 @@
+# RESTFUL API Using Python Flask and Swagger UI
+
+## A Brief Introduction
+
+In last few years REST (REpresentational State Transfer) has been used as an architectural design for web services and web APIs. The REST architecture was originally designed to fit the HTTP protocol that the world wide web uses.
+Central to the concept of RESTful web services is the notion of resources. Resources are represented by URIs. The clients send requests to these URIs using the methods defined by the HTTP protocol, and possibly as a result of that the state of the affected resource changes.
+
+Building RESTFUL web services with Flask is quite simple. There are many Flask extensions that help with building RESTful services with Flask. In place of a database we will use a memory structure. it woyld work when the web server that runs our application is single process and single threaded. it is just to be used as example with Flask's own development web server. one must not use this technique on a production web server, proper database either SQL or No-SQL depends on the usage setup must be used instead.
+
+Moreover we have used Swagger UI that allows anyone — be it your development team or your end consumers — to visualize and interact with the API’s resources without having any of the implementation logic in place.
+
+## Pre-requisites
+
+One must have Python installed in his local system for deploying this RESTFUL-API easily. Other than Python one must also have to install Python-Flask and its dependencies as mentioned in the requirements.txt file.
+
+## Running the RESTFUL-API Service
+
+```sudo python run.py``` 
+
+It will deploy the web service, no need of any manual or human intervention. 
+
+## Checking the User Interface
+
+https://localhost:5000/
---
+++ Development_Section/person.py
@@ -0,0 +1,83 @@
+from flask import make_response, abort
+
+PERSON = {
+    "john": {
+        "first_name": "john",
+        "last_name": "skeet",
+        "age": "22",
+        "favourite_color": "green",
+    },
+    "Adam": {
+        "first_name": "Adam",
+        "last_name": "Gilchrist",
+        "age": "25",
+        "favourite_color": "yellow",
+    },
+    "Donald": {
+        "first_name": "Donald",
+        "last_name": "Trump",
+        "age": "30",
+        "favourite_color": "red",
+    },
+}
+
+def get_all():
+    get_all_values = [] 
+    for key in PERSON.keys():
+        get_all_values.append(PERSON[key])
+    return get_all_values
+
+def get_one(first_name):
+    if first_name in PERSON:
+        person = PERSON.get(first_name)
+    else:
+        abort(
+            404, "Person with first name {first_name}, not found".format(first_name=first_name)
+        )
+    return person
+
+def delete(first_name):
+    if first_name in PERSON:
+        del PERSON[first_name]
+        return make_response(
+            "Person with first name {first_name} successfully deleted".format(first_name=first_name), 200
+        )
+    else:
+        abort(
+            404, "Person with first name {first_name} not found".format(first_name=first_name)
+        )
+
+def add(person):
+    first_name = person.get("first_name", None)
+    last_name = person.get("last_name", None)
+    age = person.get("age", None)
+    favourite_color = person.get("favourite_color", None)
+
+
+    if first_name not in PERSON and first_name is not None:
+        PERSON[first_name] = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "age": age,
+            "favourite_color": favourite_color,
+        }
+        return make_response(
+            "{first_name} record successfully created".format(first_name=first_name), 201
+        )
+    else:
+        abort(
+            406,
+            "Person with first name {first_name} already exists".format(first_name=first_name),
+        )
+
+def update(first_name, person):
+    if first_name in PERSON:
+        PERSON[first_name]["last_name"] = person.get("last_name")
+        PERSON[first_name]["age"] = person.get("age")
+        PERSON[first_name]["favourite_color"] = person.get("favourite_color")
+
+        return PERSON[first_name]
+    else:
+        abort(
+            404, "Person with first name {first_name} not found in the list".format(first_name=first_name)
+        )
